d00dde9a23823534c4a7b26979f0cc2f86bd9bcf
Anuj Singh <singhanuj620@gmail.com>
2022-02-02 23:22:48 +0530

    knapsack topdown

--- Recursion/Permutations.py
+++ Recursion/Permutations.py
@@ -5,6 +5,8 @@ def findP(nums, n, ind, resArr):
         return
     for i in range(ind, n):
         nums[ind], nums[i] = nums[i], nums[ind]
+        # yaha pr ind+1 issleye ho raha kyuki hamesa full array consider karna hai jo for loop ka i
+        # handle karega, i+1 tab hota jab issko chor baaki rest ko consider karna hota hai
         findP(nums, n, ind+1, resArr)
         nums[ind], nums[i] = nums[i], nums[ind]
 
