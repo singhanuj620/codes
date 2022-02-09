c7308196b0c05afdd0768a9d9b81eb4b22ec0f63
Anuj Singh <singhanuj620@gmail.com>
2022-02-10 00:08:00 +0530

    lcs : print lcs

--- DP/PrintLCS.py
+++ DP/PrintLCS.py
@@ -1,6 +1,6 @@
-str1 = 'abcdxyz'
-str2 = 'xyzabcd'
-# op = 4 i.e abcd
+str1 = 'anuj'
+str2 = 'abcuj'
+# op = auj
 
 n = len(str1)
 m = len(str2)
@@ -14,10 +14,17 @@ for i in range(1, n+1):
         if str1[i-1] == str2[j-1]:
             t[i][j] = 1 + t[i-1][j-1]
         else:
-            t[i][j] = 0
-mx = 0
-for i in range(n+1):
-    for j in range(m+1):
-        if t[i][j] > mx:
-            mx = t[i][j]
-print(mx)
+            t[i][j] = max(t[i-1][j], t[i][j-1])
+s = ""
+i = n
+j = m
+while i > 0 and j > 0:
+    if str1[i-1] == str2[j-1]:
+        s += str1[i-1]
+        i -= 1
+        j -= 1
+    elif t[i-1][j] > t[i][j-1]:
+        i -= 1
+    else:
+        j -= 1
+print(s[::-1])
